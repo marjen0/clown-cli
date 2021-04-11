a06923b0e67da7a7ea6fbe9eedf7a5f55ffb7d45
marjen0 <m.jenulis@gmail.com>
2021-04-11 11:29:44 +0300

    fix exports

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const { platforms, assetTypes, orientations, shapes } = require('./src/constants');
-const { generateFavicons } = require('./src/core/FaviconGenerator');
-const { generateLaunchIcons } = require('./src/core/IconGenerator');
-const { generateSplashScreens } = require('./src/core/SplashGenerator');
-const { generateNotificationIcon } = require('./src/core/NotificationGenerator');
-const { generateAllAssets } = require('./src/core/AssetsGenerator');
+const FaviconGenerator = require('./src/core/FaviconGenerator');
+const IconGenerator = require('./src/core/IconGenerator');
+const SplashGenerator = require('./src/core/SplashGenerator');
+const NotificationGenerator = require('./src/core/NotificationGenerator');
+const AssetsGenerator = require('./src/core/AssetsGenerator');
+
 
 exports.platforms = platforms;
 exports.assetTypes = assetTypes;
 exports.orientations = orientations;
 exports.shapes = shapes;
-exports.generateFavicons = generateFavicons;
-exports.generateLaunchIcons = generateLaunchIcons;
-exports.generateSplashScreens = generateSplashScreens;
-exports.generateNotificationIcon = generateNotificationIcon;
-exports.generateAllAssets = generateAllAssets;
+exports.FaviconGenerator = FaviconGenerator;
+exports.IconGenerator = IconGenerator;
+exports.SplashGenerator = SplashGenerator;
+exports.NotificationGenerator = NotificationGenerator;
+exports.AssetsGenerator = AssetsGenerator;
